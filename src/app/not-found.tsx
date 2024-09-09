3fe023e260d8391d6b4119d8b85dd2287fa5aac9
october <octoberserver@gmail.com>
2024-09-09 10:21:20 +0800

    .

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,8 +6,8 @@ export default function Page404() {
     <div className="h-[100vh] w-full bg-cover bg-center bg-opacity-80" style={{backgroundImage: `url(${bg.src})`}}>
       <div className="h-full w-full flex items-center text-white shadow-[inset_15rem_0_45rem_15rem_#000000A0] bg-black bg-opacity-40">
         <div className="flex flex-col gap-4 justify-start pl-[10%]">
-          <h1 className="text-[6rem] shadow-2xl">找不到頁面 :(</h1>
-          <Link href="/" className="block border-yellow-300 border-solid border-2 text-yellow-300 p-2 px-3 text-3xl rounded-lg w-fit">返回首頁</Link>
+          <h1 className="text-[6rem] shadow-2xl font-['Noto_Serif_TC']">找不到頁面 :(</h1>
+          <Link href="/" className="block border-yellow-300 border-solid border-2 text-yellow-300 p-2 px-3 text-3xl w-fit ml-4">返回首頁</Link>
         </div>
       </div>
     </div>
