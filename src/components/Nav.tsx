8bd1e8a7da12eeac92ac11acd4e1c851846f4636
october <octoberserver@gmail.com>
2024-09-09 09:56:19 +0800

    .

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,14 +4,14 @@ import "./Nav.css"
 
 function Nav() {
   return (
-    <nav className="flex justify-between h-14 fixed w-full z-50 backdrop-blur-sm shadow-md border-solid border-gray-500 border-b-[1px] border-opacity-25">
+    <nav className="flex justify-between h-14 fixed w-full z-50 backdrop-blur-sm shadow-md border-solid border-gray-500 border-b-[1px] border-opacity-25 font-['Noto_Serif_TC']">
       <div className="flex pl-8 items-center gap-4">
         <img className="block h-10 w-10 rounded-full border-solid border-2 border-[--theme-colour]" src={logoImg.src}/>
-        <h2 className="text-3xl font-['Noto_Serif_TC'] font-[900]">ZLCSC</h2>
+        <h2 className="text-3xl font-[900]">ZLCSC</h2>
       </div>
       <div className="flex">
         <div className="triangle"></div>
-        <div className="flex items-center pl-4 pr-10 bg-[--theme-colour] gap-6 h-14">
+        <div className="flex items-center pl-4 pr-10 bg-gradient-to-r from-violet-700 to-[--theme-colour] gap-6 h-14 font-semibold">
           <Link href="/" className="text-xl">首頁</Link>
           <Link href="/" className="text-xl">關於</Link>
           <Link href="/" className="text-xl">研究方向</Link>
