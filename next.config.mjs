422f731632795db196022b8a91f0ce5ade46516b
october <octoberserver@gmail.com>
2025-01-02 08:51:17 +0800

    : )

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import withMDX from "@next/mdx"
 
 /** @type {import('next').NextConfig} */
 const config = withMDX()({
+  output: "export",
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 })
 
